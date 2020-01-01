fef24926ce06846548a84afe591f0fe70d9fa47b
WolfieWaffle <wolfie.waffle11@gmail.com>
2020-01-01 17:12:37 -0500

    You can now obtain XP when cooking items

--- src/main/java/com/github/wolfiewaffle/tanspit/blocks/BlockSpit.java
+++ src/main/java/com/github/wolfiewaffle/tanspit/blocks/BlockSpit.java
@@ -10,12 +10,14 @@ import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
@@ -25,6 +27,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.items.IItemHandler;
@@ -118,6 +121,7 @@ public class BlockSpit extends BlockContainer {
 				if (!te.inventory().getStackInSlot(i).isEmpty()) {
 
 					// Take item
+					onCrafting(playerIn, te.inventory().getStackInSlot(i));
 					playerIn.inventory.addItemStackToInventory(te.inventory().getStackInSlot(i));
 					te.inventory().extractItem(i, 1, false);
 
@@ -143,6 +147,42 @@ public class BlockSpit extends BlockContainer {
 			return false;
 		}
 		return super.canPlaceBlockOnSide(worldIn, pos, side);
+	}
+
+	/**
+	 * the itemStack passed in is the output - ie, iron ingots, and pickaxes,
+	 * not ore and wood.
+	 * 
+	 * COPIED FROM SlotFuranceOutput
+	 */
+	protected void onCrafting(EntityPlayer player, ItemStack stack) {
+
+		stack.onCrafting(player.world, player, stack.getCount());
+
+		if (!player.world.isRemote) {
+			int itemCount = stack.getCount();
+			float xpFloat = FurnaceRecipes.instance().getSmeltingExperience(stack);
+
+			if (xpFloat == 0.0F) {
+				itemCount = 0;
+			} else if (xpFloat < 1.0F) {
+				int j = MathHelper.floor((float) itemCount * xpFloat);
+
+				if (j < MathHelper.ceil((float) itemCount * xpFloat) && Math.random() < (double) ((float) itemCount * xpFloat - (float) j)) {
+					++j;
+				}
+
+				itemCount = j;
+			}
+
+			while (itemCount > 0) {
+				int k = EntityXPOrb.getXPSplit(itemCount);
+				itemCount -= k;
+				player.world.spawnEntity(new EntityXPOrb(player.world, player.posX, player.posY + 0.5D, player.posZ + 0.5D, k));
+			}
+		}
+
+		net.minecraftforge.fml.common.FMLCommonHandler.instance().firePlayerSmeltedEvent(player, stack);
 	}
 
 	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
